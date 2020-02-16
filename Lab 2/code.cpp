3e54bd23f6fca862179e917b68ccd820135ec25b
Blonges <paul170101066@iitg.ac.in>
2020-02-16 13:44:02 +0530

    added newInstance() and clone() definitions of object declaration

--- Lab 2/code.cpp
+++ Lab 2/code.cpp
@@ -5,6 +5,8 @@
 // just checks using new keyword and a previously defined class name
 // can check function definitions
 // ignore if comments are there
+
+//object declaration with contructor class karna hai?? https://www.javatpoint.com/how-many-ways-to-create-an-object-in-java
 #include <bits/stdc++.h>
 using namespace std;
 bool isbackslash(char c)
@@ -54,11 +56,13 @@ int main()
   bool commentflag=false;
   bool quoteflag=false;
   unordered_map <string,int> mp;
+  unordered_map <string,int> Objmp;
   ifstream ip;
   ofstream op;
   vector<string> lines;
   string s;
-  ip.open("input.txt");
+  string t1="",t2="",t3="";
+  ip.open("fakeip.txt");
   int ln=0;   // line number
   while (ip)
   {
@@ -77,6 +81,9 @@ int main()
     bool class_flag=false;
     while(j<len)
     {
+      t3=t2;
+      t2=t1;
+      t1=t;
       t="";
       if(commentflag || quoteflag)
       {
@@ -181,7 +188,8 @@ int main()
         t+=s[j];
         j++;
       }
-      if(t=="class")
+      //cout<<"t = "<<t<<" t1 = "<<t1<<" t2 = "<<t2<<" t3 = "<<t3<<"\n";
+      if(t=="class" && mp.find(t1)==mp.end()) 
       {
         class_def++;
         if(j>=len)
@@ -189,6 +197,7 @@ int main()
           cout<<"class name not in line "<<ln<<endl;
           exit(1);
         }
+
         t="";
         while(j<len && (isspace(s[j]) || !isalnum(s[j])))
         {
@@ -217,6 +226,7 @@ int main()
       }
       else if(t=="new")
       {
+        //cout<<"found new, t1 = "<<t1<<" & t2 = "<<t2<<"\n";
         if(j>=len)
         {
           cout<<"class name not in line "<<ln<<endl;
@@ -241,8 +251,45 @@ int main()
           }
           if(s[j]=='(')
           {
+            Objmp[t1]=1;
             obj_def++;
           }
+        }
+      }
+      else if(t=="newInstance")
+      {
+        //cout<<"found newInstance, t1 = "<<t1<<" t2 = "<<t2<<" t3 = "<<t3<<"\n";
+        if(t1=="class" && mp.find(t2)!=mp.end() && j<len && s[j]=='(')
+        {
+          if(Objmp[t3]){
+            cout<<"ERROR, Object name declared more than once";
+            exit(1);
+          }
+          Objmp[t3]=1;
+          obj_def++;
+        }
+        else if(j<len && s[j]=='(')
+        {
+          obj_def++;
+        }
+      }
+      else if(t=="clone")
+      {
+        //cout<<"found clone, t1 = "<<t1<<" t2 = "<<t2<<" t3 = "<<t3<<"\n";
+        if(Objmp.find(t1)==Objmp.end())
+        {
+          cout<<"Error, parent object not defined";
+          exit(1);
+        }
+        if(mp.find(t2)!=mp.end() && j<len && s[j]=='(') //eg: Dog obj = (Dog)obj1.clone();
+        {
+          Objmp[t3]=1;
+          obj_def++;
+        }
+        else if(mp.find(t3)!=mp.end() && j<len && s[j]=='(')// eg: Dog obj = obj1.clone();
+        {
+          Objmp[t2]=1;
+          obj_def++;
         }
       }
       else if(mp.find(t)!=mp.end())
